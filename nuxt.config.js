8015efcfd68ccaeee155af748b9e18f4ae02be4c
jozsatibold <jozsatibold@gmail.com>
2023-01-19 15:52:16 +0200

    Vercel test commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,23 +1,23 @@
 export default {
   server: {
     port: 4000,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     timing: false
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Secret',
+    title: "Secret",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/css/tailwind.css'],
+  css: ["~/assets/css/tailwind.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -28,28 +28,28 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    '@nuxt/postcss8'
+    "@nuxt/typescript-build",
+    "@nuxt/postcss8"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa'
+    "@nuxtjs/pwa"
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/'
+    baseURL: "/"
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: "en"
     }
   },
 
@@ -73,6 +73,6 @@ export default {
     }
   },
   serverMiddleware: [
-    {path: '/api', prefix: false, handler: '~/serverMiddleware/api.js'}
+    { path: "/api", prefix: false, handler: "~/serverMiddleware/api.js" }
   ]
-}
+};
