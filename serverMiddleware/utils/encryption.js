268ddb63b8349e720ef5bf69f74dffc6cf7c56c9
jozsatibold <jozsatibold@gmail.com>
2023-01-19 16:30:31 +0200

    Encryption library change

--- serverMiddleware/utils/encryption.js
+++ serverMiddleware/utils/encryption.js
@@ -1,24 +1,22 @@
-const crypto = require("crypto");
-
+const CryptoJS = require("crypto-js");
 
 export const encrypt = (text, iv) => {
-  const algorithm = "aes-256-cbc";
-  const cipher = crypto.createCipheriv(
-    algorithm,
-    process.env.ENCRYPT_KEY || "c63c2c1a76653f6178ef9ff3a8748abe",
-    Buffer.from(iv.replace(/-/g, ""), 'hex')
-  );
-  let encrypted = cipher.update(text);
-  encrypted = Buffer.concat([encrypted, cipher.final()]);
-  return encrypted.toString("hex");
+  const data = text.slice();
+  const key = process.env.ENCRYPT_KEY || "c63c2c1a76653f6178ef9ff3a8748abe"
+  const encryptedString = CryptoJS.AES.encrypt(data, key, {
+    iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7
+  })
+  return encryptedString.toString();
 };
 
 export const decrypt = (encryptedText, iv) => {
-  const algorithm = "aes-256-cbc";
-  const key = process.env.ENCRYPT_KEY || "c63c2c1a76653f6178ef9ff3a8748abe";
-  const ivBf = Buffer.from(iv.replace(/-/g, ""), "hex");
-  const decipher = crypto.createDecipheriv(algorithm, key, ivBf);
-  let decrypted = decipher.update(Buffer.from(encryptedText, "hex"));
-  decrypted = Buffer.concat([decrypted, decipher.final()]);
-  return decrypted.toString();
+  const key = process.env.ENCRYPT_KEY || "c63c2c1a76653f6178ef9ff3a8748abe"
+  const decrypted = CryptoJS.AES.decrypt(encryptedText, key, {
+    iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7
+  });
+  return decrypted.toString(CryptoJS.enc.Utf8)
 };
